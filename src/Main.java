cfee891e9c26f053eafd3f8306d85ea0f049ba31
Nora <nhalmarri64@gmail.com>
2024-11-06 11:15:50 +0300

    leap year task

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter a year: ");
+        int year = input.nextInt();
+
+
+//divisible by 4 but not by 100, unless it is also divisible by 400.
+
+        if((year % 4 == 0) &&  (year%100 != 0) && (year%400 == 0 )){
+            System.out.printf(" %d is a leap year",year);
+        }else {
+            System.out.printf(" %d are NOT a leap year",year);
+        }
+
+
+
+
+    } //end of main class
+}
