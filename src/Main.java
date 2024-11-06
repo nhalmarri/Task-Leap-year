7dff7475602977cf62755c6836a794c8bf69a025
Nora <nhalmarri64@gmail.com>
2024-11-06 11:19:29 +0300

    updated

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
+            Scanner input = new Scanner(System.in);
 
-        System.out.println("Please enter a year: ");
-        int year = input.nextInt();
+            System.out.println("Please enter a year: ");
+            int year = input.nextInt();
 
 
 //divisible by 4 but not by 100, unless it is also divisible by 400.
 
-        if((year % 4 == 0) &&  (year%100 != 0) && (year%400 == 0 )){
-            System.out.printf(" %d is a leap year",year);
-        }else {
-            System.out.printf(" %d are NOT a leap year",year);
-        }
-
-
+            if((year % 4 == 0) &&  (year%100 != 0) || (year%400 == 0 )){
+                System.out.printf(" %d is a leap year",year);
+            }else {
+                System.out.printf(" %d are NOT a leap year",year);
+            }
 
 
     } //end of main class
